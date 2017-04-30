62449508053bb2a49763e855ef75e70107a7c653
전형탁 <andy395023@gmail.com>
2017-04-30 15:21:34 +0900

    Create gradient_descent_method.py

---
+++ learning neural network/gradient_descent_method.py
@@ -0,0 +1,8 @@
+def gradient_descent(f, init_x, lr=0.01, step_num=100):
+    x = init_x
+    
+    for i in range(step_num):
+        grad = numerical_gradient(f, x)
+        x -= lr * grad
+        
+    return x
